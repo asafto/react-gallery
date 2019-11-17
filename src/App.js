32299053ad3edbcdfd7b158d861a87025bb642c9
Asaf Toledano <asafto@gmail.com>
2019-11-17 12:18:45 +0200

    Gallery 1st version

--- src/App.js
+++ src/App.js
@@ -3,14 +3,50 @@ import './App.css';
 
 class App extends React.Component {
 
-  render() {
-    return (
-        <div className="App">
-          <h1>Select an album:</h1>
+    constructor(props) {
+        super(props);
+        this.state = {
+            albums: [],
+            images: []
+        };
+    }
 
-        </div>
-    );
-  }
+    componentDidMount() {
+        fetch(`https://jsonplaceholder.typicode.com/albums`)
+            .then(res => res.json())
+            .then(albums => {
+                this.setState({albums});
+            });
+    }
+
+    getAlbumImages(e) {
+        fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${e.target.value}`)
+            .then(res => res.json())
+            .then(images => {
+                this.setState({images});
+        });
+}
+
+    render() {
+        return (
+            <div className="App">
+              <h1>Select an album:</h1>
+                <select onChange={this.getAlbumImages.bind(this)} className="albumSelectBox">
+                    <option label="Select..." />
+                    {this.state.albums.map(album => {
+                        return <option value={album.id} key={album.id}>{album.title}</option>
+                    })}</select>
+                <hr/>
+                <div className="album-images">
+                    {this.state.images.map(image => {
+                        return <a href={image.url}>
+                            <img src={image.thumbnailUrl} alt={image.title} />
+                        </a>
+                    })}
+                </div>
+            </div>
+        );
+    }
 
 }
 
